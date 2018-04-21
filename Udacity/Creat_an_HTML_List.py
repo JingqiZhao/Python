92986a914e10c349ac1fdec23e3799e57317b5ac
JingqiZhao <qi.panst@gmail.com>
2018-04-21 18:06:37 +0900

    Create Creat_an_HTML_List.py

---
+++ Udacity/Creat_an_HTML_List.py
@@ -0,0 +1,20 @@
+#Write a for loop that iterates over a list of strings and creates a single string, html_str, which is an HTML list. For example, if the list is items = ['first string', 'second string'], printing html_str should output:
+
+#<ul>
+#<li>first string</li>
+#<li>second string</li>
+#</ul>
+#That is, the string's first line should be the opening tag <ul>. Following that is one line per element in the source list, surrounded by <li> and </li> tags. The final line of the string should be the closing tag </ul>.
+
+
+
+items = ['first string', 'second string']
+
+# write your for loop here
+html_str = ('<ul>' + '\n')
+
+for item in items:
+    html_str += ('<li>' + item + '</li>' + '\n')
+
+html_str += ('</ul>')
+print (html_str)
